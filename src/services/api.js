3abfb2c870d59111c2570b210b1aaa890492a320
RitikSuthar07 <ritiksuthar.7905@gmail.com>
2026-04-18 19:48:48 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://url-shortner-backend-y0u9.onrender.com/api/',
+  baseURL: 'https://url-shortner-backend-y0u9.onrender.com/',
 });
 
 // attach token automatically
